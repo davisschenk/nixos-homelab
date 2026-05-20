92a24cd1859787581bce3da7c59d85a78f701fc1
Davis <davis@example.com>
2026-05-19 18:19:55 -0700

    feat: Jellyfin with Intel QSV iGPU transcoding

--- modules/nixos/media.nix
+++ modules/nixos/media.nix
@@ -1,1 +1,20 @@
-{ ... }: { }
+{ pkgs, ... }:
+{
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # iHD driver for Gen8+ (UHD 770)
+      intel-compute-runtime # OpenCL support
+    ];
+  };
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = false; # Caddy handles access
+  };
+
+  users.users.jellyfin.extraGroups = [
+    "render"
+    "video"
+  ];
+}
