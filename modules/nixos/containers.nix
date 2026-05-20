8d347c2688f785a56c0b16e6a2069574e0b35138
Davis <davis@example.com>
2026-05-19 18:38:34 -0700

    feat: Romm oci-container

--- modules/nixos/containers.nix
+++ modules/nixos/containers.nix
@@ -1,1 +1,22 @@
-{ ... }: { }
+{ ... }:
+{
+  virtualisation.docker.enable = true;
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+
+    containers.romm = {
+      image = "rommapp/romm:latest";
+      autoStart = true;
+      ports = [ "127.0.0.1:8888:8080" ];
+      volumes = [
+        "/persist/containers/romm/data:/romm/data"
+        "/persist/containers/romm/config:/romm/config"
+        "/data/media/roms:/romm/library"
+      ];
+      environment = {
+        ROMM_BASE_PATH = "/romm";
+      };
+    };
+  };
+}
