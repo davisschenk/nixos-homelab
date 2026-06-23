011d0d0e6801f0c6f00621f9ed806a18e79cca11
Davis <davis@example.com>
2026-06-23 08:09:55 -0700

    fix: add initrd kernel modules to hardware-configuration.nix

--- hosts/mangrove/hardware-configuration.nix
+++ hosts/mangrove/hardware-configuration.nix
@@ -1,1 +1,11 @@
-_: { }
+_: {
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "ahci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
+  boot.kernelModules = [ "kvm-amd" ];
+}
