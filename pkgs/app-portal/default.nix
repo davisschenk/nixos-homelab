c9cac6cb1b047e5a196b063766f08f59e791929b
Davis Schenkenberger <davisschenk@gmail.com>
2026-09-20 18:04:26 -0600

    Remove Starline app

--- pkgs/app-portal/default.nix
+++ pkgs/app-portal/default.nix
@@ -1,28 +1,8 @@
-{
-  buildNpmPackage,
-  runCommand,
-}:
-let
-  starline = buildNpmPackage {
-    pname = "star-technology-lines";
-    version = "0.1.0";
-    src = ../../apps/star-technology-lines;
-    npmDepsHash = "sha256-vD6pzaQgW8Mx0dFyLtBcOt8w0KYfH+JaAF2AW8Q2AD4=";
-    VITE_BASE_PATH = "/starline/";
-
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out
-      cp -r dist/. $out/
-      runHook postInstall
-    '';
-  };
-in
+{ runCommand }:
 runCommand "homelab-app-portal" { } ''
-  mkdir -p $out/starline
+  mkdir -p $out
   cp ${../../apps/portal/index.html} $out/index.html
   cp ${../../apps/portal/main.js} $out/main.js
   cp ${../../apps/portal/styles.css} $out/styles.css
   cp ${../../apps/portal/apps.json} $out/apps.json
-  cp -r ${starline}/. $out/starline/
 ''
