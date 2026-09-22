5a52c7dfef516b55f5f4ed25a812e662547b7799
Davis Schenkenberger <davisschenk@gmail.com>
2026-09-22 17:09:57 -0600

    Update Star Technology to Theta 2 Hotfix 2

--- hosts/mangrove/default.nix
+++ hosts/mangrove/default.nix
@@ -87,7 +87,7 @@
       # installs Forge automatically. https://www.curseforge.com/minecraft/modpacks/star-technology
       environment = {
         PROJECT_ID = "924189";
-        VERSION_ID = "8914563"; # StarT Theta 2 Hotfix 1 Server Files
+        VERSION_ID = "8949465"; # StarT Theta 2 Hotfix 2 Server Files
       };
       secretEnvironmentFile = config.sops.templates."curseforge-environment".path;
 
